ea8604279b3f8a1e1938d285d3046a79c91b8d01
ltestard2021 <laurent.testard2021@campus-eni.fr>
2021-09-17 17:01:55 +0200

    création du projet

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,7 +13,7 @@ class MainController extends AbstractController
      */
     public function home(): Response
     {
-        echo 'test Jessy';
+        echo 'test Laurent';
         die();
         //return $this->render('main/index.html.twig';
     }
