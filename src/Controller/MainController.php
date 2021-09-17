67971689109f85c571e467d091c6871adc94e9d8
ltestard2021 <laurent.testard2021@campus-eni.fr>
2021-09-17 17:03:18 +0200

    premier commit test laurent

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -13,7 +13,7 @@ class MainController extends AbstractController
      */
     public function home(): Response
     {
-        echo 'test Laurent';
+        echo 'test Laurent ';
         die();
         //return $this->render('main/index.html.twig';
     }
