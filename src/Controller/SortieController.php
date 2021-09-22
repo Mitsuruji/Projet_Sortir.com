1acc5258b83660be8e0eaaed1ac57c6d3813a4b1
Ryknot <seg_jes@hotmail.com>
2021-09-22 16:23:36 +0200

    inscription sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Entity\Sortie;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,8 +19,36 @@ class SortieController extends AbstractController
     public function sortie(SortieRepository $sortieRepository): Response
     {
         $sorties = $sortieRepository->findSearch();
+
         return $this->render('sortie/list_sorties.html.twig', [
-            'sorties' => $sorties,
+            'sorties' => $sorties
         ]);
     }
+
+    /**
+     * @Route("/sortie/{idSortie}_{idParticipant}/inscription", name="sortie_inscription")
+     */
+    public function inscriptionSortie(int $idParticipant,int $idSortie, ParticipantRepository $participantRepository, EntityManagerInterface $entityManager): Response
+    {
+        try {
+            //récupération instance Participant et Sortie
+            $sorties = $entityManager->getRepository(Sortie::class)->find($idSortie);
+            $participant = $participantRepository->find($idParticipant);
+
+            //insert BDD (table: participant_sortie)
+            $sorties->addParticipantInscrit($participant);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Inscription à la sortie réussi ! Amusez-vous bien !');
+            return $this->render('main/home.html.twig');
+
+        }catch (\Exception $e){
+            $this->addFlash('warning', $e->getMessage());
+            return $this->render('main/home.html.twig');
+        }
+    }
+
+
 }
